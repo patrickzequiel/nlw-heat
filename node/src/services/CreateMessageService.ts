4932bf5c55518a56a84cedcca6ea27ee9509fb6d
Patrick Cruz <patrickezequielscruz@gmail.com>
2021-10-22 11:20:48 +0200

    Finalizado aplicação no react native

--- node/src/services/CreateMessageService.ts
+++ node/src/services/CreateMessageService.ts
@@ -15,17 +15,17 @@ class CreateMessageService {
     });
 
 
-    const infoWs = {
+    const infoWS = {
+      id: message.id,
       text: message.text,
-      user_id: message.user.id,
       created_at: message.created_at,
       user: {
         name: message.user.name,
-        avatar_url: message.user.avatar_url
-      }
-    }
-    
-    io.emit('new message', infoWs)
+        avatar_url: message.user.avatar_url,
+      },
+    };
+
+    io.emit("new_message", infoWS);
 
     return message;
   }
